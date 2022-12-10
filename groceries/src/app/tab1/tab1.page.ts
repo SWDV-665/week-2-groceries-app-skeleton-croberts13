96e4089f1f8b9bc16a79fb68d30c4aa68fa58944
croberts13 <croberts16@live.maryville.edu>
2022-12-10 18:09:48 -0500

    working ion-sliding , delete , add

--- groceries/src/app/tab1/tab1.page.ts
+++ groceries/src/app/tab1/tab1.page.ts
@@ -1,71 +1,55 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
-
+import { GroceriesServicesService } from '../groceries.service';
+import { InputDialogService } from '../input-dialog.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  title = "Grocery List";
-  items = [
-    {
-      name: "Milk",
-      quantity: 2
-    },
-    {
-      name: "Bread",
-      quantity: 3
-    },
-    {
-      name: "Eggs",
-      quantity: 12
-    },
-    {
-      name: "Juice",
-      quantity: 1
-    },
-  ];
-  constructor( public toastCtrl: ToastController, public alertCtrl: AlertController) {
 
-  }
-  removeItem(item, index) {
-    console.log("Removing Item -", item, index);
-    const toast = this.toastCtrl.create({
-      message: 'Removing Item -' + index + "...",
-      duration: 3000
-    });
-   
+  
 
-   this.items.splice(index, 1);
+  constructor(
+    public toastCtrl: ToastController,
+    public alertController: AlertController,
+    public dataService: GroceriesServicesService,
+    public inputDialogService: InputDialogService
+  ) {}
 
-  }
-  addItem() {
-    console.log("Adding Item");
-    this.presentAlert();
-  }
-  async presentAlert() {
-    const alert = await this.alertCtrl.create({
-      header: 'Please enter Grocery Item',
-      buttons: ['Save'],
-      inputs: [
-        {
-          placeholder: 'Item',
-        },
-        {
-          type: 'number',
-          placeholder: 'Quantity',
-          min: 1,
-          max: 100,
-        },
-        
-      ],
-      
-    });
 
-    await alert.present();
-  }
+  loadItems() {
+  return this.dataService.getItems();
+}
+addItem() {
+  console.log('Adding Item to List...');
+  this.inputDialogService.presentAlert();
 }
 
+/*the Edit Grocery Item*/
+async editItem(n, index) {
+  console.log('Edit Item: ', n, index);
+  const toast = this.toastCtrl.create({
+    message: 'Updating Item to List: ' + n.itemName,
+    duration: 3000
+  });
+  (await toast).present();
+  this.inputDialogService.presentAlert(n, index);
+}
+
+
+
+/*the Remove Grocery Item*/
+async removeItem(n, index) {
+  console.log('Remove This Item: ', n, index);
+  const toast = this.toastCtrl.create({
+    message: 'Deleting Item - ' + n.itemName + ' ...',
+    duration: 3000
+});
+  (await toast).present();
+  this.dataService.removeItem(index);
+}
+}
